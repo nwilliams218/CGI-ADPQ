8f756f74273af5b7a16bf8f9756bbb2b9e3565e5
aneesh.pakala <aneesh.pakala@CGI-D32150.ams.com>
2016-06-04 11:04:43 -0700

    Modified changes and added update profile

--- services/ProfileService/src/main/java/com/cgi/chhs/adpq/service/profile/service/micro/ProfileService.java
+++ services/ProfileService/src/main/java/com/cgi/chhs/adpq/service/profile/service/micro/ProfileService.java
@@ -16,70 +16,116 @@ import com.cgi.chhs.adpq.service.profile.entity.Profile;
 
 @Component
 
-public class ProfileService extends CGIService{
-	
-	
+public class ProfileService extends CGIService {
+
 	@Inject
 	public ProfileRepository repository;
 
-	@Path("/view/{id}")
+	@Path("/viewprofile/{id}")
 	@GET
-    @Produces("application/json")
-	public Profile view(@PathParam("id") Long id)
-	{
-		
+	@Produces("application/json")
+	public Profile view(@PathParam("id") Long id) {
+
 		return repository.findOne(id);
 	}
-	
+
 	@Path("/updateProfile")
 	@POST
-    @Produces("application/json")
-	public SaveStatus editProfile(@RequestBody Profile profileObj){			
-		
+	@Produces("application/json")
+	public SaveStatus editProfile(@RequestBody Profile profileObj) {
+
 		SaveStatus editStatus = new SaveStatus();
-		try{
-		Profile profileDBObj = repository.findOne(profileObj.getId());	
-		profileDBObj.setPassword(profileObj.getPassword());
-		profileDBObj.setAddress(profileObj.getAddress());
-		profileDBObj.setContact(profileObj.getContact());
-		profileDBObj.setEmail(profileObj.getEmail());	
-		
-		repository.save(profileDBObj);
-		editStatus.setSuccess(true);
-		editStatus.setMessage("Profile updated  successfully and a notification has been sent to the case worker.");
-		return editStatus;
-		}catch(Exception e)
-		{
+		try {
+
+			if (repository.exists(profileObj.getId())) {
+				Profile profileDBObj = repository.findOne(profileObj.getId());
+				profileDBObj.setPassword(profileObj.getPassword());
+				profileDBObj.setAddress(profileObj.getAddress());
+				profileDBObj.setContact(profileObj.getContact());
+				profileDBObj.setEmail(profileObj.getEmail());
+				repository.save(profileDBObj);
+				editStatus.setSuccess(true);
+				editStatus.setMessage(
+						"Profile updated  successfully and a notification has been sent to the case worker.");
+			} else {
+				editStatus.setSuccess(false);
+				editStatus.setMessage("Profile couldnt be found !!");
+			}
+			return editStatus;
+		} catch (Exception e) {
 			editStatus.setSuccess(false);
 			editStatus.setMessage("Profile updation failed and notification has been sent to the case worker.");
 			return editStatus;
 		}
-		
-		
+
 	}
-	
 
 	@Path("/addProfile")
 	@POST
-    @Produces("application/json")
-	public SaveStatus addProfile(@RequestBody Profile profileObj)
-	{	
+	@Produces("application/json")
+	public SaveStatus addProfile(@RequestBody Profile profileObj) {
 		SaveStatus saveStatus = new SaveStatus();
-		
-		try{
+
+		try {
 			repository.save(profileObj);
 			saveStatus.setSuccess(true);
 			saveStatus.setMessage("Profile added successfully and a notification has been sent to the case worker.");
-			saveStatus.setSavedObjectId(profileObj.getId());			
+			saveStatus.setSavedObjectId(profileObj.getId());
 			return saveStatus;
-		}catch(Exception e){
+		} catch (Exception e) {
 			saveStatus.setSuccess(false);
 			saveStatus.setMessage("Adding  profile failed, a notification has been sent to the case worker.");
 			return saveStatus;
-			
+
 		}
-		
-		
+
+	}
+
+	@Path("/readProfile")
+	@POST
+	@Produces("application/json")
+	public Profile readProfile(@RequestBody Profile profileObj) {
+		Profile profileDBObj = null;
+		try {
+			profileDBObj = repository.findOne(profileObj.getId());
+			return profileDBObj;
+		} catch (Exception e) {
+
+			return profileDBObj;
+
+		}
+
 	}
 	
+	@Path("/delProfile/{id}")
+	@GET
+	@Produces("application/json")
+	public SaveStatus delProfile(@PathParam("id") Long id) {
+		SaveStatus saveStatus = new SaveStatus();
+		try {
+
+			if (repository.exists(id))
+			{
+				repository.delete(id);
+				saveStatus.setSuccess(true);
+				saveStatus.setMessage(
+						"Profile deleted successfully and a notification has been sent to the case worker.");
+				saveStatus.setSavedObjectId(id);
+			} else {
+				saveStatus.setSuccess(false);
+				saveStatus.setMessage("Profile couldnt be found ");
+				saveStatus.setSavedObjectId(id);
+
+			}
+			return saveStatus;
+		} catch (Exception e) {
+
+			saveStatus.setSuccess(false);
+			saveStatus.setMessage("Adding  profile failed, a notification has been sent to the case worker.");
+			return saveStatus;
+
+		}
+
+	}
+
 }
