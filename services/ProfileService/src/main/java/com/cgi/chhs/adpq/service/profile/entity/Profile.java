cfbba23b1a5fe40ac1a1ebce16574f4c1fa6fe89
Michael Dominick <michael@buccaneertech.com>
2016-06-07 13:56:41 -0400

    Changed profile model at front-ends request

--- services/ProfileService/src/main/java/com/cgi/chhs/adpq/service/profile/entity/Profile.java
+++ services/ProfileService/src/main/java/com/cgi/chhs/adpq/service/profile/entity/Profile.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Date;
 
 @Entity
 public class Profile {
@@ -14,8 +15,133 @@ public class Profile {
 	private String password;
 	private String email;
 	private String address;
-	private Integer householdId;
-	//TODO: Add more fields as needed
+	private String address2;
+	private String zip;
+	private String phone;
+	private String location;
+	private String facility;
+	private String relationship;
+	private String gender;
+	private Boolean group;
+	private String goal;
+	private Date dob;
+	private String firstName;
+	private String lastName;
+	private String profilePicture;
+	private long parentId;
+
+	public long getParentId() {
+		return parentId;
+	}
+
+	public void setParentId(long parentId) {
+		this.parentId = parentId;
+	}
+
+	public String getProfilePicture() {
+		return profilePicture;
+	}
+
+	public void setProfilePicture(String profilePicture) {
+		this.profilePicture = profilePicture;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	public String getGoal() {
+		return goal;
+	}
+
+	public void setGoal(String goal) {
+		this.goal = goal;
+	}
+
+	public Boolean getGroup() {
+		return group;
+	}
+
+	public void setGroup(Boolean group) {
+		this.group = group;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getRelationship() {
+		return relationship;
+	}
+
+	public void setRelationship(String relationship) {
+		this.relationship = relationship;
+	}
+
+	public String getFacility() {
+		return facility;
+	}
+
+	public void setFacility(String facility) {
+		this.facility = facility;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getAddress2() {
+		return address2;
+	}
+
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+
 
 	public long getId() {
 		return id;
