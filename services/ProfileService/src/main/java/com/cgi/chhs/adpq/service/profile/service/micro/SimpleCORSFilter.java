68dc5f4e79db0c6c267d497145c322e2c6adbc7d
Michael Dominick <michael@buccaneertech.com>
2016-06-06 22:52:01 -0400

    Removed unused imports

--- services/ProfileService/src/main/java/com/cgi/chhs/adpq/service/profile/service/micro/SimpleCORSFilter.java
+++ services/ProfileService/src/main/java/com/cgi/chhs/adpq/service/profile/service/micro/SimpleCORSFilter.java
@@ -1,18 +1,11 @@
 package com.cgi.chhs.adpq.service.profile.service.micro;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.CorsFilter;
-
-import javax.servlet.*;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import javax.servlet.Filter;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.core.MultivaluedMap;
-import java.util.logging.LogRecord;
 
 /**
  * Created by michael on 6/6/16.
