e2c44efe48848737f22502f9ff16947489fab8fc
steve sickman <sickman@gmail.com>
2016-06-09 15:56:26 -0700

    fix issue on checking empty confirm passwords

--- client/src/user/RegisterController.js
+++ client/src/user/RegisterController.js
@@ -15,8 +15,7 @@
 		$scope.register = function() {
 			$scope.passwordsMatch = true;
 			
-			
-			if (!$scope.credentials.password || $scope.credentials.confirm) {
+			if (!$scope.credentials.password || !$scope.credentials.confirm) {
 				postman.warn(gettextCatalog.getString('Password is required'));
 			} else if ($scope.credentials.password != $scope.credentials.confirm) {
 				$scope.passwordsMatch = false;	
